429faf3a7abdb5b25b998a9ee1a6b2ea5cf45b48
OvenTD <155340997+OvenTD@users.noreply.github.com>
2024-07-26 17:25:40 +0900

    Create get_folders.py

---
+++ get_folders.py
@@ -0,0 +1,16 @@
+import os
+
+def print_folder_names(path):
+    try:
+        # 선택한 경로에서 모든 항목을 검사
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+            # 해당 항목이 폴더인지 확인
+            if os.path.isdir(item_path):
+                print(item)
+    except Exception as e:
+        print(f"Error: {e}")
+
+# 사용할 경로를 지정
+selected_path = 'D:/Hijackers_Gdrive/Asset_reference/Square'
+print_folder_names(selected_path)
